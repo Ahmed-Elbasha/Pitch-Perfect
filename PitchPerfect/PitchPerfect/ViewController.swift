f54810c86e8e0ff83f302b4f7bd30cfee45ccdb1
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-06-01 17:54:52 +0200

    optimized AutoLayout of first ViewController

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Ahmed Elbasha on 5/31/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording button was pressed")
+    }
+}
+
